cd0ff4a0ff0800a8cbaec1846e8099a148677f1a
NouZen22 <104245456+NouZen22@users.noreply.github.com>
2022-04-24 12:40:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['6285748894171'] // Put your number here
+global.owner = ['62895705062107'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -47,7 +47,7 @@ global.author = `Nakataa`
 global.wm = 'Nakataa' //Nama Bot
 global.watermark = wm
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
-global.instagram = 'https://instagram.com/nakataa1.a'
+global.instagram = 'https://instagram.com/fhriahm.a'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
